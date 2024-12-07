3662fddfe467cca7012dcf9029ed01f5d888943f
diogomr <diogo.moreira@amplemarket.com>
2024-12-07 11:52:51 +0000

    day07

---
+++ day07/main.py
@@ -0,0 +1,71 @@
+import itertools
+
+
+def calc(columns, operators):
+    ret = columns[0]
+    for idx in range(len(operators)):
+        if operators[idx] == "+":
+            ret += columns[idx + 1]
+        elif operators[idx] == "*":
+            ret *= columns[idx + 1]
+        elif operators[idx] == "||":
+            ret = int(str(ret) + str(columns[idx + 1]))
+        else:
+            raise Exception(f"Invalid op {operators[idx]}")
+
+    return ret
+
+
+def part1():
+    lines = open('input.txt').read().splitlines()
+
+    ans = 0
+    for line in lines:
+        test_value, columns = line.split(": ")
+        test_value = int(test_value)
+        columns = list(map(int, columns.split(" ")))
+
+        ops = []
+        for prod in itertools.product(["+", "*"], repeat=len(columns) - 1):
+            op = []
+            for idx in range(len(columns) - 1):
+                op.append(prod[idx])
+
+            ops.append(op)
+
+        for op in ops:
+            if calc(columns, op) == test_value:
+                ans += test_value
+                break
+
+    return ans
+
+
+def part2():
+    lines = open('input.txt').read().splitlines()
+
+    ans = 0
+    for line in lines:
+        test_value, columns = line.split(": ")
+        test_value = int(test_value)
+        columns = list(map(int, columns.split(" ")))
+
+        ops = []
+        for prod in itertools.product(["+", "*", "||"], repeat=len(columns) - 1):
+            op = []
+            for idx in range(len(columns) - 1):
+                op.append(prod[idx])
+
+            ops.append(op)
+
+        for op in ops:
+            if calc(columns, op) == test_value:
+                ans += test_value
+                break
+
+    return ans
+
+
+if __name__ == "__main__":
+    print(f"Part 1: {part1()}")
+    print(f"Part 2: {part2()}")
